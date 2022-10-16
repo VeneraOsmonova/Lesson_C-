78b6a150ab08ada26f225d445a1e439f86bbe65a
Venera <osmven@gmail.com>
2022-10-16 19:11:09 +0300

    Exm005_ConditionIfElse/

---
+++ Exm005_ConditionIfElse/Program.cs
@@ -0,0 +1,17 @@
+﻿/*● Начало
+● Ввести имя пользователя
+● ЕСЛИ имя пользователя совпадает с “Маша” то… поприветствовать по особенному
+● ИНАЧЕ поприветствовать по обычному
+● Конец*/
+
+System.Console.WriteLine("Введите имя пользователя: ");
+string username = Console.ReadLine();
+
+if (username.ToLower() == "masha")
+{
+    System.Console.WriteLine("Ура, это же Masha!");
+}
+else
+{
+    System.Console.WriteLine("Привет, " + username + "!");
+}
