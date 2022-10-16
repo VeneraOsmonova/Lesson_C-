ac584d1eedbe2b6db77346a6eeb15bf4d2df03ef
Venera <osmven@gmail.com>
2022-10-16 19:02:29 +0300

    Exm003_Sum/

---
+++ Exm003_Sum/Program.cs
@@ -0,0 +1,16 @@
+﻿/*● Начало
+● Определить первое число
+● Определить второе число
+● Вывести сумму введенных чисел на экран
+● Конец*/
+
+/*System.Console.WriteLine("Введите два числа: ");
+int num1 = Convert.ToInt32(Console.ReadLine());
+int num2 = Convert.ToInt32(Console.ReadLine());
+int summ = num1 + num2;
+System.Console.WriteLine($"summa = {summ}");*/
+
+int num1 = 6;
+int num2 = 5;
+int summ = num1 + num2;
+System.Console.WriteLine($"summa = {summ}");
