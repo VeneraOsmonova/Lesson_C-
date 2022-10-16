ec0b086c6bdf4e9cde837cb9bc6ba43377f071bc
Venera <osmven@gmail.com>
2022-10-16 18:57:39 +0300

    Exm002_HelloUser/

---
+++ Exm002_HelloUser/Program.cs
@@ -0,0 +1,9 @@
+﻿/*● Начало
+● Ввести <имя пользователя>
+● Вывести “Привет, ”
+● Вывести <имя пользователя>
+● Конец*/
+
+System.Console.WriteLine("Введите ваше имя: ");
+string username = Console.ReadLine();
+System.Console.WriteLine("Привет, " + username + "!");
